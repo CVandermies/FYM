981826ca511b5954a1459f5e88110c33c42ff1b8
CVandermies <charlesvdm@outlook.com>
2021-03-23 09:48:31 +0100

    create movie form

--- src/Controller/FymController.php
+++ src/Controller/FymController.php
@@ -6,6 +6,8 @@ use App\Entity\Movie;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FymController extends AbstractController
@@ -33,14 +35,34 @@ class FymController extends AbstractController
         ]);
     }
 
+    
+    /**
+     * @Route("/fym/new", name="movie_create")
+     */
+    public function createMovie(Request $request, EntityManagerInterface $manager){
+        $movie = new Movie();
+
+        $form = $this->createFormBuilder($movie)
+                        ->add('title',)
+                        ->add('content',)
+                        ->add('rating',)
+                        ->add('image',)
+                        ->add('category',)
+                        ->getForm();
+
+        return $this->render("fym/createMovie.html.twig", [
+            'formMovie' => $form->createView()
+        ]);    
+    }
+
     /**
      * @Route("/fym/movie/{id}", name="movie_show")
      */
 //show concerned movie note
-    public function showMovie(Movie $movie){
+public function showMovie(Movie $movie){
        
-        return $this->render("fym/showMovie.html.twig", [
-            'movie' => $movie
-        ]);
-    }
+    return $this->render("fym/showMovie.html.twig", [
+        'movie' => $movie
+    ]);
+}
 }
